df1cc0c83a97b85ce0489f9240a9cb1a0db53715
alain <alainlay@gmail.com>
2019-05-26 18:45:17 +1200

    add menu to entries activity

--- app/src/main/java/com/laynet/passwordmanager/activities/EntriesActivity.java
+++ app/src/main/java/com/laynet/passwordmanager/activities/EntriesActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -70,6 +71,13 @@ public class EntriesActivity extends AppCompatActivity {
             entries.addAll(retainedFragment.getData());
         }
         mapEntriesToListView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
     private void readEntriesFromFile() throws IOException, CryptoException {
